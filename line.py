8cf4e42211c2a71fe3c32f2505413c9d6652b467
Mertcan Cakir <zestillest@gmail.com>
2023-01-03 00:51:50 +0300

    Update line.py

--- line.py
+++ line.py
@@ -4,59 +4,83 @@ Created on Wed Nov  3 23:10:38 2021
 
 @author: Mertcan
 """
-
+import sys
 import cv2
 import numpy as np
 
-hl = 250
+aRRAYH = 250
 
-img = np.ones((hl,hl),np.uint8)
+ball = {"x":20, "y":3, "xIncreasing":2, "yIncreasing":3, "updown":False, "leftright":False}
 
-# x is the x value of the ball's starting point
-x = 2
-# y is the y value of the ball's starting point
-y = 3
+"""
+    x: x coordinate of the ball
+    y: y coordinate of the ball
+    xIncreasing: x coordinate of the ball will increase by this value
+    yIncreasing: y coordinate of the ball will increase by this value
+    updown: reverses whether the ball is going down or up
+    leftright: reverses whether the ball is going left or right
+"""
 
-# n is the increment of x
-n = 3
-# m is the increment of y
-m = 2
+for i in sys.argv:
+    if i == "-size":
+        aRRAYH = int(sys.argv[sys.argv.index(i)+1])
+    elif i == "-s":
+        aRRAYH = int(sys.argv[sys.argv.index(i)+1])
+    elif i == "-x":
+        ball["x"] = int(sys.argv[sys.argv.index(i)+1])
+    elif i == "-y":
+        ball["y"] = int(sys.argv[sys.argv.index(i)+1])
+    elif i == "--xIncreasing":
+        ball["xIncreasing"] = int(sys.argv[sys.argv.index(i)+1])
+    elif i == "-xI":
+        ball["xIncreasing"] = int(sys.argv[sys.argv.index(i)+1])
+    elif i == "--yIncreasing":
+        ball["yIncreasing"] = int(sys.argv[sys.argv.index(i)+1])
+    elif i == "-yI":
+        ball["yIncreasing"] = int(sys.argv[sys.argv.index(i)+1])
+    elif i == "--updown":
+        ball["updown"] = sys.argv[sys.argv.index(i)+1]
+    elif i == "-uD":
+        ball["updown"] = sys.argv[sys.argv.index(i)+1]
+    elif i == "--leftright":
+        ball["leftright"] = sys.argv[sys.argv.index(i)+1]
+    elif i == "-lR":
+        ball["leftright"] = sys.argv[sys.argv.index(i)+1]
+    elif i == "--help" or i == "-h":
+        print("Usage: python line.py [options]")
+        print("Example: python line.py -s 500 -x 20 -y 3 --xIncreasing 2 --yIncreasing 3")
+        print("Options:")
+        print("  -size or -s: size of the window")
+        print("  -x: x coordinate of the ball")
+        print("  -y: y coordinate of the ball")
+        print("  --xIncreasing or xI: x coordinate of the ball will increase by this value")
+        print("  --yIncreasing or yI: y coordinate of the ball will increase by this value")
+        print("  --help or -h: show this help message and exit")
+        sys.exit(0)
 
-# These four variables represent the state of hitting a wall.
-ust = 0
-alt = 0
-sag = 0
-sol = 0
+img = np.ones((aRRAYH,aRRAYH),np.uint8)
 
-while True:
-  
-    if x <= 0:
-        sol = not sol
+def videoLoop():
+    if ball["x"] <= 0 or ball["x"] >= aRRAYH:
+        ball["leftrigth"] = not ball["leftright"]
+        ball["xIncreasing"] = -ball["xIncreasing"]
+        
+    if ball["y"] <= 0 or ball["y"] >= aRRAYH:
+        ball["updown"] = not ball["updown"]
+        ball["yIncreasing"] = -ball["yIncreasing"]
     
-    if x >= hl:
-        sag = not sag
-        
-    if y <= 0:
-        ust = not ust
-        
-    if y >= hl:
-        alt = not alt
-
-    if ust == 1 or alt == 1:
-        n = -n
-
-    if sag == 1 or sol == 1:
-        m = -m
-
-    x  = x + m
-    y = y + n
-    print(n, alt)
-    cv2.circle(img,(x,y),1,255,-1)
+    ball["x"] = ball["x"] + ball["xIncreasing"]
+    ball["y"] = ball["y"] + ball["yIncreasing"]
+    
+    cv2.circle(img,(ball["x"], ball["y"]),1,255,-1)
     
     cv2.imshow("window",img)
 
     key = cv2.waitKey(1) & 0xFF
     if key == ord("q"):
-        break
+        return 0
+    
+    return videoLoop()
 
+videoLoop()
 cv2.destroyAllWindows()
